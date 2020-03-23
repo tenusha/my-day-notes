db35586e8df3efd955e7246d69f87d2fcd42e55f
vimukthi <tenushamadhushan@gmail.com>
2020-03-23 08:08:03 +0530

    Add note

--- lib/AddNote.dart
+++ lib/AddNote.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_day/Login.dart';
+import 'package:my_day/Note.dart';
+import 'package:my_day/NoteAPI.dart';
 import 'package:my_day/ThemeData.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,6 +12,11 @@ class AddNotePage extends StatefulWidget {
 
 class _AddNotePageState extends State<AddNotePage> {
   String username = "";
+  String collectionName = 'Notes';
+
+  bool enableSaveIcon = true;
+  TextEditingController subject = new TextEditingController();
+  TextEditingController note = new TextEditingController();
 
   @override
   void initState() {
@@ -34,36 +41,104 @@ class _AddNotePageState extends State<AddNotePage> {
     }
   }
 
-  handleSaveClick() {
+  handleSaveClick(BuildContext context) async {
+    _disableButtons();
     print('handleSaveClick()');
+    try {
+      if (subject.text == null || subject.text == "") {
+        _showToast(context, "The subject is empty");
+      } else if (note.text == null || note.text == "") {
+        _showToast(context, "The note is empty. Please add something");
+      } else {
+        Note noteObj = new Note(
+            subject: subject.text,
+            note: note.text,
+            username: username,
+            timestamp: new DateTime.now().millisecondsSinceEpoch);
+        bool status = await addNote(collectionName, noteObj);
+        if(status){
+          Navigator.of(context).pop();
+        }else{
+          _showToast(context, "Unable to add the note. Please chech your connection");
+        }
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+    _enableButtons();
+  }
+
+  _disableButtons() {
+    setState(() {
+      enableSaveIcon = false;
+    });
+  }
+
+  _enableButtons() {
+    setState(() {
+      enableSaveIcon = true;
+    });
+  }
+
+  void _showToast(BuildContext context, String text) {
+    final scaffold = Scaffold.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: Text(text),
+        action: SnackBarAction(
+            label: 'Ok', onPressed: scaffold.hideCurrentSnackBar),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Add new note",
-            style: TextStyle(color: Colors.white, fontSize: 16)),
+        title: Text("Add Note", style: TextStyle(color: Colors.white)),
         centerTitle: false,
         backgroundColor: themeColor,
         iconTheme: IconThemeData(
           color: Colors.white,
         ),
         actions: [
-          IconButton(
-            icon: Icon(Icons.save),
-            onPressed: () {
-              handleSaveClick();
-            },
-          ),
+          Builder(builder: (BuildContext context) {
+            return new IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                enableSaveIcon ? handleSaveClick(context) : print('disabled');
+              },
+            );
+          }),
           SizedBox(width: 10),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Text('Add Note'),
-            Text(username),
+            Padding(
+                padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+                child: TextField(
+                    controller: subject,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration.collapsed(
+                        hintText: "Subject",
+                        hintStyle: new TextStyle(fontSize: 16.0)),
+                    style: new TextStyle(fontSize: 20.0, color: Colors.black))),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: Divider(color: Colors.grey, thickness: 0.4),
+            ),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: note,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  minLines: 20,
+                  decoration:
+                      InputDecoration.collapsed(hintText: "Add your note here"),
+                )),
           ],
         ),
       ),
---
+++ lib/NoteAPI.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:my_day/Note.dart';
+
+import 'User.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+getNotes(String collectionName, String username) {
+  return Firestore.instance
+      .collection(collectionName)
+      .where('username', isEqualTo: username)
+      .getDocuments()
+      .then((QuerySnapshot docs) {
+    print('getNotes()');
+    if (docs.documents.isEmpty) {
+      print('No existing notes');
+      return null;
+    } else {
+      print('existing notes found');
+      return docs.documents;
+    }
+  });
+}
+
+addNote(String collectionName, Note note) async {
+  try {
+    Firestore.instance.runTransaction((Transaction transaction) async {
+      await Firestore.instance
+          .collection(collectionName)
+          .document()
+          .setData(note.toJson());
+    });
+  } catch (e) {
+    print(e.toString());
+    return false;
+  }
+  return true;
+}
+
+update(Note note, Note newNote) {
+  try {
+    Firestore.instance.runTransaction((transaction) async {
+      await transaction.update(note.reference, newNote.toJson());
+    });
+  } catch (e) {
+    print(e.toString());
+  }
+}
+
+delete(Note note) {
+  Firestore.instance.runTransaction((Transaction transaction) async {
+    await transaction.delete(note.reference);
+  });
+}
